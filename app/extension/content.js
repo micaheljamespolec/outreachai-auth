8efd0d83816a8d989012c3981d0bee72e19fc107
SourcedOut <admin@sourcedout.ai>
2026-04-25 15:35:47 -0400

    Remove app/ folder - cleanup

--- app/extension/content.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
-  if (message.type !== 'scrape') return false
-  sendResponse({ linkedin_url: window.location.href.split('?')[0] })
-  return false
-})
